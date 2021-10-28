5b6041cf9673de53cb0d40cce711223a9383928c
andrej <andrejschwanke19@gmail.com>
2021-10-28 18:40:56 +0200

    ex2

---
+++ ProgrammingCourse/Exercise01_304/Problem1.3/Divide.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  double a, b;
+
+  // ask user to enter two numbers and store values in a and b
+  cout << "Enter two numbers: ";
+  cin >> a >> b;
+
+  // calculate a divided by b and print out result
+  double x = a/b;
+  cout << a << " divided by " << b << " is equal to " << x << endl;
+
+}
