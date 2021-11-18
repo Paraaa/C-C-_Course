5603193219cce0c847a593ee56857ec5911e1f3e
Andrej Schwanke <andrejschwanke19@gmail.com>
2021-11-18 19:54:54 +0100

    p4.2

--- ProgrammingCourse/Exercise04_304/Problem4.2/Statistics.cpp
+++ ProgrammingCourse/Exercise04_304/Problem4.2/Statistics.cpp
@@ -4,18 +4,36 @@
 
 using namespace std;
 
+double mean(double arr[], int);
+double stddev(double arr[], int, double);
+
 int main(int argc, char *argv[]){
     int num_val = argc - 1;
     double *nums = new double[num_val];
 
     for(int i = 1; i <= num_val; i++){
-        cout << argv[i] << endl;
         nums[i-1] = atof(argv[i]);
     }
- 
-    for(int i = 0; i < num_val; i++){
-        cout << nums[i] << endl;
-    }
+
+    double m = mean(nums, num_val);
+    cout << "The mean is: " << m << endl;
+    double stdv = stddev(nums, num_val, m);
+    cout << "The stddev is: " << stdv << endl;
 }
 
-double mean(double arr)
+double mean(double arr[] , int N){
+    double sum = 0.0;
+    for(int i = 0; i < N; i++){
+        sum += arr[i];
+    }
+    return sum/N;
+}
+
+double stddev(double arr[] , int N, double m){
+    double sum = 0.0;
+    for(int i = 0; i < N; i++){
+        sum += (arr[i] - m) * (arr[i] - m); 
+    }
+    double s = sqrt(sum / (N - 1));
+    return s;
+}
